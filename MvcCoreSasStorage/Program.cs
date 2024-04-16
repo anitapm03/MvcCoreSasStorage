40b2f8eb41bcf3fe116cbaa62576b27882984d9c
anitapm03 <apereiramolinero@gmail.com>
2024-04-16 13:55:06 +0200

    .

--- MvcCoreSasStorage/Program.cs
+++ MvcCoreSasStorage/Program.cs
@@ -1,6 +1,9 @@
+using MvcCoreSasStorage.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<HelperXml>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
